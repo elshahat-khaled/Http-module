043118f581af4fb4ac28efd11966c9422c269751
elshahat-khaled <elshahat.khaled@iisgalvanimi.edu.it>
2022-04-04 08:25:25 +0000

    fine

--- khaled-http/src/app/foo/foo.component.ts
+++ khaled-http/src/app/foo/foo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { foo } from './foo.model';
 
 @Component({
  selector: 'app-foo',
@@ -8,31 +9,43 @@ import { Observable } from 'rxjs';
  styleUrls: ['./foo.component.css']
 })
 export class FooComponent {
-   data: Object = undefined!;
-   loading: boolean = undefined!;
-   o :Observable<Object>= undefined!;
-   constructor(public http: HttpClient) {} 
+  fooData! : foo[];
+  data!: Object;
+  loading!: boolean;
+  o!: Observable<Object>;
+  ofoo! : Observable<foo[]>;
+  constructor(public http: HttpClient) { }
 
-   makeRequest(): void {
-    //Notifichiamo che stiamo attendendo dei dati
-    this.loading = true; 
-    //Facciamo una get e otteniamo l'oggetto Observable che attende la risposta
+  makeRequest(): void {
+    
+    this.loading = true;
     this.o = this.http.get('https://jsonplaceholder.typicode.com/posts/1');
-    //Attacchiamo all'Observable o un metodo "observer" che verrà lanciato quando arriva la 
-    //risposta
     this.o.subscribe(this.getData);
   }
-  //Il metodo che notifica la risposta (nota che usiamo una "arrow function")
-  getData = (d : Object) =>
-  {
-    this.data = d; //Notifico l’oggetto ricevuto dal server
-    this.loading = false;  // Notifico che ho ricevuto i dati
+
+  getData = (d: Object) => {
+    this.data = d;
+    this.loading = false;
   }
+
+  //Nota bene, questo è un metodo alternativo al metodo makeRequest
+  makeCompactRequest(): void {
+    this.loading = true;
+    this.http
+      .get('https://jsonplaceholder.typicode.com/posts/1')
+      .subscribe(data => {
+        this.data = data;
+        this.loading = false;
+      });
+  }
+
+  //L'operazione di post necessita un parametro in più.
+  //Viene creata una stringa (JSON.strigify) a partire da un oggetto Typescript
   makeCompactPost(): void {
     this.loading = true;
     this.http
       .post('https://jsonplaceholder.typicode.com/posts',
-        JSON.stringify({
+        JSON.stringify({ 
           body: 'bar',
           title: 'foo',
           userId: 1
@@ -43,4 +56,15 @@ export class FooComponent {
         this.loading = false;
       });
   }
+
+  makeTypedRequest() : void
+  {
+    //oFoo : Observable<Foo[]>; va dichiarato tra gli attributi della classe 
+    this.ofoo = this.http.get<foo[]>('https://jsonplaceholder.typicode.com/posts');
+    this.ofoo.subscribe(data => {this.fooData = data;});
+  }
+
+  ngOnInit() {
+  }
+
 }
